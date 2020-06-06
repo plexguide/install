92d700531743ae78b60c2952944d0bd06f433205
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2020-06-06 18:17:30 -0400

    update

--- install.sh
+++ install.sh
@@ -14,11 +14,22 @@ EOF
 common_confirm() {
   read -p 'Confirm Information | Press [Enter] ' typed </dev/tty
 }
+
+common_fcreate_silent() {
+    mkdir -p "$1"
+    chown 1000:1000 "$1"
+    chmod 0775 "$1"
+}
+
 # EXECUTION ####################################################################
-mkdir -p /pg/tmp/install /pg/var
-rm -rf /pg/tmp/install/
+common_fcreate_silent /pg/tmp/install
+common_fcreate_silent /pg/var
+
+rm -rf /pg/tmp/install/*
 git clone -b v1 --single-branch https://github.com/plexguide/install.git /pg/tmp/install
 cp /pg/tmp/install/pgupdate /bin/
+chmod 0755 /pg/pgupdate
+chown 1000:1000 /pgupdate
 
 common_header "🚀 Initial Install Complete - Type [pgupdate] to start!"
 common_confirm
